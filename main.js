d4a703ee1b9b5dc8170bf7b223137a7568743807
Luluzaogamer <147192785+Luluzaogamer@users.noreply.github.com>
2024-04-17 08:59:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function botao()
 {
-  
+  texto = document.getElementById("texto").indexHTML;
+  document.getElementById(texto).index.html="<h2 id='texto'>pronto, agora você passou de todas as fases!!!</h2>";
 }
